0611f3758cd3e14bc5dcb429f4c41ca45f52496a
atsuta1120 <cocoa201153@gmail.com>
2026-06-03 16:16:40 +0900

    Initial Commit

---
+++ Yarikiri_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yarikiri_app
+//
+//  Created by KAZUKI ATSUTA on 2026/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yarikiri_app/Yarikiri_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yarikiri_appApp.swift
+//  Yarikiri_app
+//
+//  Created by KAZUKI ATSUTA on 2026/06/03.
+//
+
+import SwiftUI
+
+@main
+struct Yarikiri_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
